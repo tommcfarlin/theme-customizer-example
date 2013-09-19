6f6c8e28b6c339d8a093c84f60bb072bdd1dfc52
Tom McFarlin <tom@tommcfarlin.com>
2013-09-19 16:28:32 -0400

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,7 +1,6 @@
-
 		<div id="footer">
 			&copy; <?php echo date( 'Y' ); ?> <?php bloginfo( 'title' ); ?> All Rights Reserved
 		</div><!-- /#footer -->
-
+                <?php wp_footer(); ?>
 	</body>
 </html>
