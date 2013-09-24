384c4f5be5df04e33deaa1d1282fbbbf3d702378
Tom McFarlin <tom@tommcfarlin.com>
2013-09-24 11:49:00 -0400

    adding the section, setting, and control for the 'Display Header' setting

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@
  * @param      object    $wp_customize    The WordPress Theme Customizer
  * @package    tcx
  * @since      0.2.0
- * @version    0.2.0
+ * @version    0.4.0
  */
 function tcx_register_theme_customizer( $wp_customize ) {
 
@@ -30,13 +30,49 @@ function tcx_register_theme_customizer( $wp_customize ) {
 		)
 	);
 
+	/*-----------------------------------------------------------*
+	 * Defining our own section
+	 *-----------------------------------------------------------*/
+
+	$wp_customize->add_section(
+		'tcx_display_options',
+		array(
+			'title'     => 'Display Options',
+			'priority'  => 200
+		)
+	);
+
+	/* Display Header */
+	$wp_customize->add_setting(
+		'tcx_display_header',
+		array(
+			'default'    =>  'true',
+			'transport'  =>  'postMessage'
+		)
+	);
+
+	$wp_customize->add_control(
+		'tcx_display_header',
+		array(
+			'section'   => 'tcx_display_options',
+			'label'     => 'Display Header?',
+			'type'      => 'checkbox'
+		)
+	);
+
 } // end tcx_register_theme_customizer
 add_action( 'customize_register', 'tcx_register_theme_customizer' );
 
 function tcx_customizer_css() {
 ?>
 	 <style type="text/css">
+
 	     a { color: <?php echo get_theme_mod( 'tcx_link_color' ); ?>; }
+
+	     <?php if( false === get_theme_mod( 'tcx_display_header' ) ) { ?>
+	     	#header { display: none; }
+	     <?php } // end if ?>
+
 	 </style>
 <?php
 }
@@ -55,7 +91,7 @@ function tcx_customizer_live_preview() {
 		'tcx-theme-customizer',
 		get_template_directory_uri() . '/js/theme-customizer.js',
 		array( 'jquery', 'customize-preview' ),
-		'0.3.0',
+		rand(), // 0.4.0
 		true
 	);
 
