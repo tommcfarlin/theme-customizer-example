967e49f3896dbd73bf2917c477272e0078a01fc9
Tom McFarlin <tom@tommcfarlin.com>
2013-09-25 12:16:19 -0400

    adding new options to the theme customizer

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@
  * @param      object    $wp_customize    The WordPress Theme Customizer
  * @package    tcx
  * @since      0.2.0
- * @version    0.4.0
+ * @version    0.2.0
  */
 function tcx_register_theme_customizer( $wp_customize ) {
 
@@ -60,18 +60,113 @@ function tcx_register_theme_customizer( $wp_customize ) {
 		)
 	);
 
+	/* Change Color Scheme */
+	$wp_customize->add_setting(
+		'tcx_color_scheme',
+		array(
+			'default'   => 'normal',
+			'type'      => 'option',
+			'transport' => 'postMessage'
+		)
+	);
+
+	$wp_customize->add_control(
+		'tcx_color_scheme',
+		array(
+			'section'  => 'tcx_display_options',
+			'label'    => 'Color Scheme',
+			'type'     => 'radio',
+			'choices'  => array(
+				'normal'    => 'Normal',
+				'inverse'   => 'Inverse'
+			)
+		)
+	);
+
+	/* Change Font */
+	$wp_customize->add_setting(
+		'tcx_font',
+		array(
+			'default'   => 'times',
+			'transport' => 'postMessage'
+		)
+	);
+
+	$wp_customize->add_control(
+		'tcx_font',
+		array(
+			'section'  => 'tcx_display_options',
+			'label'    => 'Theme Font',
+			'type'     => 'select',
+			'choices'  => array(
+				'times'     => 'Times New Roman',
+				'arial'     => 'Arial',
+				'courier'   => 'Courier New'
+			)
+		)
+	);
+
+	/* Display Copyright */
+	$wp_customize->add_setting(
+		'tcx_footer_copyright_text',
+		array(
+			'default'            => 'All Rights Reserved',
+			'sanitize_callback'  => 'tcx_sanitize_copyright',
+			'transport'          => 'postMessage'
+		)
+	);
+
+	$wp_customize->add_control(
+		'tcx_footer_copyright_text',
+		array(
+			'section'  => 'tcx_display_options',
+			'label'    => 'Copyright Message',
+			'type'     => 'text'
+		)
+	);
+
 } // end tcx_register_theme_customizer
 add_action( 'customize_register', 'tcx_register_theme_customizer' );
+
+/**
+ * Sanitizes the incoming input and returns it prior to serialization.
+ *
+ * @param      string    $input    The string to sanitize
+ * @return     string              The sanitized string
+ * @package    tcx
+ * @since      0.5.0
+ * @version    0.1.0
+ */
+function tcx_sanitize_copyright( $input ) {
+	return strip_tags( stripslashes( $input ) );
+} // end tcx_sanitize_copyright
 
 function tcx_customizer_css() {
 ?>
 	 <style type="text/css">
 
+		 body {
+
+		 	font-family: <?php echo get_theme_mod( 'tcx_font' ); ?>
+
+		 	<?php if ( 'normal' === get_theme_mod( 'tcx_color_scheme' ) ) { ?>
+
+			 	background: #000;
+			 	color:      #fff;
+
+		 	<?php } else { ?>
+
+			 	background: #fff;
+			 	color:      #000;
+
+		 	<?php } // end if/else ?>
+		 }
+
 	     a { color: <?php echo get_theme_mod( 'tcx_link_color' ); ?>; }
 
-	     <?php if( false === get_theme_mod( 'tcx_display_header' ) ) { ?>
-	     	#header { display: none; }
-	     <?php } // end if ?>
+		<?php if( false === get_theme_mod( 'tcx_display_header' ) ) { ?>
+			#header { display: none; }
+		<?php } // end if ?>
 
 	 </style>
 <?php
@@ -91,7 +186,7 @@ function tcx_customizer_live_preview() {
 		'tcx-theme-customizer',
 		get_template_directory_uri() . '/js/theme-customizer.js',
 		array( 'jquery', 'customize-preview' ),
-		rand(), // 0.4.0
+		'0.5.0'
 		true
 	);
 
