65b76181bb0d7c29e2395db55aa7f4b25289869c
Tom McFarlin <tom@tommcfarlin.com>
2013-10-02 09:58:03 -0400

    changing the anchor's target

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
 	<div id="content">
-		This is the content. <a href="#">This is an anchor</a> so that we can tell the Theme Customizer is working.
+		This is the content. <a href="javascript:;">This is an anchor</a> so that we can tell the Theme Customizer is working.
 	</div><!-- /#content -->
 
 <?php get_footer(); ?>
